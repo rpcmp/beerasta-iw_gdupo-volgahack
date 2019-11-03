13a394cd6e8cd1fce33ee81a24378679c30daf2b
Xeruvimov <xeruvimov@gmail.com>
2019-11-03 10:29:22 +0400

    send beer

--- src/components/beer/add/add.js
+++ src/components/beer/add/add.js
@@ -4,6 +4,56 @@ import './add.scss';
 class Add extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            address: "",
+            min: 0,
+            max: 0,
+            time: ""
+        }
+
+        this.setAddress = this.setAddress.bind(this)
+        this.setMin = this.setMin.bind(this)
+        this.setMax = this.setMax.bind(this)
+        this.setTime = this.setTime.bind(this)
+        this.sendItem = this.sendItem.bind(this)
+    }
+
+    setAddress(address){
+        this.setState({address: address})
+    }
+
+    setMin(min){
+        this.setState({min: min})
+    }
+
+    setMax(max){
+        this.setState({max: max})
+    }
+
+    setTime(time){
+        this.setState({time: time})
+    }
+
+    // https://beerasta.herokuapp.com/user/personal
+    // PUT
+    //{address, min, max, time}
+
+    sendItem(){
+        let body = JSON.stringify(this.state)
+        console.table(this.state)
+
+        fetch("https://beerasta.herokuapp.com/user/personal", {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }).then(res => {
+            if (res.ok){
+                console.log("vse zaebis")
+            }
+        })
     }
 
     render() {
@@ -14,13 +64,13 @@ class Add extends React.Component {
                 <div className="form">
                     <div className="item">
                         <p>Адрес</p>
-                        <input className="input input-text"></input>
+                        <input className="input input-text" onInput={e => this.setAddress(e.target.value)}></input>
                     </div>
                     <div className="item">
                         <p>Количество человек</p>
                         <div className="half-container">
-                            <input className="input input-half" placeholder="Min"></input>
-                            <input className="input input-half" placeholder="Max"></input>
+                            <input className="input input-half" placeholder="Min" type="number" onInput={e => this.setMin(e.target.value)}></input>
+                            <input className="input input-half" placeholder="Max" type="number" onInput={e => this.setMax(e.target.value)}></input>
                         </div>
 
                     </div>
@@ -30,10 +80,10 @@ class Add extends React.Component {
                     </div>
                     <div className="item">
                         <p>Время начала</p>
-                        <input type="time" className="input"></input>
+                        <input type="time" className="input" onInput={e => this.setTime(e.target.value)}></input>
                     </div>
                     <div className="item">
-                        <button className="input-submit">Пируем</button>
+                        <button className="input-submit" onClick={this.sendItem()}>Пируем</button>
                     </div>
                 </div>
             </div>
